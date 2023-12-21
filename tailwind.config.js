b4d36aace3bcdd04030781815672b976a51c3c60
leoTechnologyDeveloper <www.leotechnology@gmail.com>
2023-12-21 05:07:36 -0500

    Improve image route

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,10 +7,10 @@ export default {
         lobster: ["Lobster", "cursive"],
       },
       backgroundImage: {
-        back1: "url('./src/assets/back1.jpg')",
-        back2: "url('./src/assets/back2.jpg')",
+        back1: "url('/src/assets/back1.jpg')",
+        back2: "url('/src/assets/back2.jpg')",
         back3:
-          "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('./src/assets/back1.jpg')",
+          "linear-gradient(to right bottom, rgba('#7ed56f',0.8), rgba('#28b485',0.8)), url('/src/assets/back1.jpg')",
       },
     },
   },
